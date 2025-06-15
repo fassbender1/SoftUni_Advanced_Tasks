d67db82b4fd4112302f7c35ffba7414628c863cf
fassbender1 <civello6969@gmail.com>
2025-06-15 06:19:22 +0300

    g

--- Tasks/Advanced/file_handling/file_reader.py
+++ Tasks/Advanced/file_handling/file_reader.py
@@ -1,3 +1,18 @@
+import os.path
+
 from Advanced.constants import abs_project_path
 
-path = open(abs_project_path)
+path = os.path.join(abs_project_path, "lab_files", "text.txt")
+file = open(path)
+
+# numbers = [int(el) for el in file.read().split("\n")]
+# print(sum(numbers))
+
+total_sum = 0
+
+for el in file.readlines():
+    if el.endswith("\n"):
+        el = el[:-1]
+    total_sum += int(el)
+
+print(total_sum)
