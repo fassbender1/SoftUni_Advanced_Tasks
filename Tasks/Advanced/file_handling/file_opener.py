b1a880be17f0a624f2099e452544df74e86d121d
fassbender1 <civello6969@gmail.com>
2025-06-15 05:46:25 +0300

    g

--- Tasks/Advanced/file_handling/file_opener.py
+++ Tasks/Advanced/file_handling/file_opener.py
@@ -1,6 +1,12 @@
-import os
+from constants import abs_project_path
 
-path = os.path.join("..", "lab_files", "text.txt")
+# # using relative path
+# import os
+
+# path = os.path.join("..", "lab_files", "text.txt")
+
+#using absolute path
+path = os.path.join(abs_project_path, "lab_files", "text.txt")
 
 try:
     open(path)
