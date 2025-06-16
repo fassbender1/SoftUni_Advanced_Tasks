43424b1ae0273546cd08f948e6f16589417e20bb
fassbender1 <civello6969@gmail.com>
2025-06-16 03:14:43 +0300

    g

--- Tasks/Advanced/file_handling/directory_traversal.py
+++ Tasks/Advanced/file_handling/directory_traversal.py
@@ -1,12 +1,31 @@
 import os
 
-from Advanced.constants import abs_project_path
+# from Advanced.constants import abs_project_path
 
 file_info = {}
 
-directory = 0
+directory = "../lab_files/"
 
-for el in os.listdir(path = os.path.join(abs_project_path,"lab_files")):
-    file_info = el
+# for el in os.listdir(path = os.path.join(abs_project_path,"lab_files")):
 
-print(file_info)
+for el in os.listdir(directory):
+    f = os.path.join(directory, el)
+    if os.path.isfile(f):
+        extension = el.split(".")[-1]
+        if extension not in file_info:
+            file_info[extension] = []
+        file_info[extension].append(el)
+    else:
+        for element in os.listdir(f):
+            file_name = os.path.join(f, element)
+            if os.path.isfile(file_name):
+                ext = element.split(".")[-1]
+                if ext not in file_info:
+                    file_info[ext] = []
+                file_info[ext].append(element)
+
+with open(os.path.join(directory, "report.txt"), "w") as output_file:
+    for ext, filenames in sorted(file_info.items()):
+        output_file.write(f".{ext}\n")
+        for file_name in sorted(filenames):
+            output_file.write(f"- - - {file_name}\n")
