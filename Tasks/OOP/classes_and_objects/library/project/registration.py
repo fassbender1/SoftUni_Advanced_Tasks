a3d8dbe0b8ffd9b203c3e2d3654de28060be33c0
fassbender1 <civello6969@gmail.com>
2025-07-05 18:31:25 +0300

    g

--- Tasks/OOP/classes_and_objects/library/project/registration.py
+++ Tasks/OOP/classes_and_objects/library/project/registration.py
@@ -1,2 +1,18 @@
+from project.user import User
+from project.library import Library
+
+
 class Registration:
-    
+    def add_user(self, user: User, library: Library):
+        if user in library.user_records:
+            return f"User with id = {user.user_id} already registered in the library!"
+        library.user_records.append(user)
+        return None
+
+    def remove_user(self, user: User, library: Library):
+        if user in library.user_records:
+            library.user_records.remove(user)
+        return f"We could not find such user to remove!"
+
+    def change_username(self, user_id: int, new_username: str, library: Library):
+        
