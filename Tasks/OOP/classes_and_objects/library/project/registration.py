c6f74d9cc3077f6fc6e2eb71af880574e62736c0
fassbender1 <civello6969@gmail.com>
2025-07-06 23:41:25 +0300

    g

--- Tasks/OOP/classes_and_objects/library/project/registration.py
+++ Tasks/OOP/classes_and_objects/library/project/registration.py
@@ -3,6 +3,7 @@ from project.library import Library
 
 
 class Registration:
+
     def add_user(self, user: User, library: Library):
         if user in library.user_records:
             return f"User with id = {user.user_id} already registered in the library!"
@@ -15,4 +16,17 @@ class Registration:
         return f"We could not find such user to remove!"
 
     def change_username(self, user_id: int, new_username: str, library: Library):
-        
+        user = next((usr for usr in library.user_records if usr.user_id == user_id), None)
+        if user is None:
+            return f"There is no user with id = {user_id}!"
+        if user.username == new_username:
+            return f"Please check again the provided username - it should be different than the username used so far!"
+        if user.username in library.rented_books:
+            library.rented_books[new_username] = library.rented_books[user.username]
+            library.rented_books.pop(user.username)
+
+        user.username = new_username
+        return f"Username successfully changed to: {new_username} for user id: {user_id}"
+
+
+
