7380d532730451a8e15bcae1ada99009ac33f0bb
fassbender1 <civello6969@gmail.com>
2025-07-07 05:08:54 +0300

    g

---
+++ Tasks/OOP/inheritance/multilevel_inheritance/main.py
@@ -0,0 +1,6 @@
+from project.sports_car import SportsCar
+
+drive = SportsCar()
+print(drive.move())
+print(drive.drive())
+print(drive.race())
---
+++ Tasks/OOP/inheritance/multiple_inheritance/main.py
@@ -0,0 +1,8 @@
+from project.teacher import Teacher
+
+function = Teacher()
+
+print(function.sleep())
+print(function.teach())
+print(function.get_fired())
+print(function)
