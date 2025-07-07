1fe44702927d9f06c5a8e545180568202100fee5
fassbender1 <civello6969@gmail.com>
2025-07-07 06:50:16 +0300

    g

--- Tasks/OOP/classes_and_objects/spoopify/project/album.py
+++ Tasks/OOP/classes_and_objects/spoopify/project/album.py
@@ -10,19 +10,19 @@ class Album:
         if self.published:
             return f"Cannot add songs. Album is published."
         if song.single:
-            return f"Cannot add {song.name}. It's a single."
-        if song.name in self.songs:
+            return f"Cannot add {song.name}. It's a single"
+        if song in self.songs:
             return f"Song is already in the album."
-        self.songs.append(song.name)
+        self.songs.append(song)
         return f"Song {song.name} has been added to the album {self.name}."
 
     def remove_song(self, song_name: str):
-        if song_name not in self.songs:
-            return f"Song is not in the album."
         if self.published:
             return f"Cannot remove songs. Album is published."
-        self.songs.remove(song_name)
-        return f"Removed song {song_name} from album {self.name}."
+        for song in self.songs:
+            self.songs.remove(song)
+            return f"Removed song {song_name} from album {self.name}."
+        return f"Song is not in the album."
 
     def publish(self):
         if self.published:
@@ -30,15 +30,15 @@ class Album:
         self.published = True
         return f"Album {self.name} has been published."
 
-    # def details(self):
-    #     result = f"Album {self.name}\n"
-    #     second_part = "\n".join(f"== {song}" for song in self.songs)
-    #     return result + second_part
     def details(self):
-        result = [f"Album {self.name}"]
-        for song in self.songs:
-            result.append(f"== {song}")
-        return "\n".join(result)
+        result = f"Album {self.name}\n"
+        second_part = "\n".join(f"== {song.get_info()}" for song in self.songs)
+        return result + second_part
+    # def details(self):
+    #     result = [f"Album {self.name}"]
+    #     for song in self.songs:
+    #         result.append(f"== {song.get_info()}")
+    #     return "\n".join(result)
 
 
 
--- Tasks/OOP/classes_and_objects/spoopify/project/band.py
+++ Tasks/OOP/classes_and_objects/spoopify/project/band.py
@@ -1,32 +1,39 @@
 from project.album import Album
 
 class Band:
-    albums = []
     def __init__(self, name: str):
         self.name = name
+        self.albums = []
 
     def add_album(self, album: Album) -> str:
-        if album.name in Band.albums:
+        if album.name in self.albums:
             return f"Band {self.name} already has {album.name} in their library."
-        Band.albums.append(album.name)
+        self.albums.append(album.name)
         return f"Band {self.name} has added their newest album {album.name}."
 
-    def remove_album(self, album_name: str) -> str:
-        if Album.published:
-            return f"Album has been published. It cannot be removed."
-        if album_name not in Band.albums:
-            return f"Album {album_name} is not found."
-        Band.albums.remove(album_name)
-        return f"Album {album_name} has been removed."
+    # def remove_album(self, album_name: str) -> str:
+    #     if Album.published:
+    #         return f"Album has been published. It cannot be removed."
+    #     if album_name not in Band.albums:
+    #         return f"Album {album_name} is not found."
+    #     Band.albums.remove(album_name)
+    #     return f"Album {album_name} has been removed."
+    def remove_album(self, album_name: str):
+        for album in self.albums:
+            if album.name == album_name:
+                if album.published:
+                    return "Album has been published. It cannot be removed."
+                self.albums.remove(album)
+                return f"Album {album_name} has been removed."
+        return f"Album {album_name} is not found."
 
     def details(self) -> str:
         result = f"Band {self.name}\n"
-        second_part = album.details()
+        second_part = "\n".join(album.details() for album in self.albums)
         return result + second_part
-
     # def details(self):
     #     result = [f"Band {self.name}"]
     #     for album in self.albums:
-    #         result.append(Album.details())
+    #         result.append(album.details())
     #     return "\n".join(result)
 
