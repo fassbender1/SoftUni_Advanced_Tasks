6ce8c01bf060cc9d5db7bd9be9f06e671a6dccd5
fassbender1 <civello6969@gmail.com>
2025-07-07 05:34:16 +0300

    g

---
+++ Tasks/OOP/inheritance/stack_of_strings.py
@@ -0,0 +1,42 @@
+class Stack:
+    def __init__(self, *args):
+        self.data = []
+
+        for el in args:
+            if isinstance(el, str):
+                self.data.append(el)
+
+    def push(self, element):
+        if isinstance(element, str):
+            self.data.append(element)
+
+    def pop(self):
+        return self.data.pop()
+
+    def top(self):
+        return self.data[-1]
+
+    def is_empty(self):
+        return len(self.data) == 0
+
+    def __str__(self):
+        result = reversed(self.data)
+        return f"[{', '.join(result)}]"
+
+s = Stack("a", "b", "c")
+print(s)
+s.push("d")
+print(s)
+print(s.top())
+print(s)
+print(s.pop())
+print(s)
+print(s.is_empty())
+print(s.pop())
+
+print(s.pop())
+
+print(s)
+print(s.is_empty())
+print(s.pop())
+print(s.is_empty())
