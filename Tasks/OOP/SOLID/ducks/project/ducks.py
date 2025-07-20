1035d94ff42ae735277b7f203f873b9b6825f2f4
fassbender1 <civello6969@gmail.com>
2025-07-21 00:27:54 +0300

    g

--- Tasks/OOP/SOLID/ducks/project/ducks.py
+++ Tasks/OOP/SOLID/ducks/project/ducks.py
@@ -14,21 +14,14 @@ class Duck(ABC):
     def fly():
         pass
 
+class Toy(ABC):
+    @abstractmethod
+    def make_sound(self):
+        pass
 
-class RubberDuck(Duck):
-    @staticmethod
-    def quack():
-        return "Squeek"
-
-    @staticmethod
-    def walk():
-        """Rubber duck can walk only if you move it"""
-        raise Exception('I cannot walk by myself')
-
-    @staticmethod
-    def fly():
-        """Rubber duck can fly only if you throw it"""
-        raise Exception('I cannot fly by myself')
+class RubberDuck(Toy):
+    def make_sound(self):
+        return f"Squeak!"
 
 
 class RobotDuck(Duck):
