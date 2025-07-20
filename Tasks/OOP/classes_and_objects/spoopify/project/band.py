a0ba1d8fcde237ded3c617cd0e41ca747abaa966
fassbender1 <civello6969@gmail.com>
2025-07-20 19:00:58 +0300

    g

--- Tasks/OOP/classes_and_objects/spoopify/project/band.py
+++ Tasks/OOP/classes_and_objects/spoopify/project/band.py
@@ -3,12 +3,12 @@ from project.album import Album
 class Band:
     def __init__(self, name: str):
         self.name = name
-        self.albums = []
+        self.albums: list[Album] = []
 
     def add_album(self, album: Album) -> str:
-        if album.name in self.albums:
+        if album in self.albums:
             return f"Band {self.name} already has {album.name} in their library."
-        self.albums.append(album.name)
+        self.albums.append(album)
         return f"Band {self.name} has added their newest album {album.name}."
 
     # def remove_album(self, album_name: str) -> str:
