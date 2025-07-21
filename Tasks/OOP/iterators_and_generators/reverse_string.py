ff4bb78071a66429d88762a0c6be84bf3d7bf182
fassbender1 <civello6969@gmail.com>
2025-07-21 06:56:57 +0300

    g

--- Tasks/OOP/iterators_and_generators/reverse_string.py
+++ Tasks/OOP/iterators_and_generators/reverse_string.py
@@ -1,6 +1,10 @@
 def reverse_text(string: str):
-    for char in reversed(string):
-        yield char
+    for ch in reversed(string):
+        yield ch
+    # index = len(string)-1
+    # while index >= 0:
+    #     yield string[index]
+    #     index -= 1
 
 for char in reverse_text("step"):
     print(char, end='')
