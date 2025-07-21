caa154cecd9c0e61b91915bd853b1c6891838576
fassbender1 <civello6969@gmail.com>
2025-07-21 06:52:05 +0300

    g

--- Tasks/OOP/iterators_and_generators/generator_range.py
+++ Tasks/OOP/iterators_and_generators/generator_range.py
@@ -1,5 +1,8 @@
 def genrange(start: int, end: int):
-    for num in range(start, end + 1):
-        yield num
+    # for num in range(start, end + 1):
+    #     yield num
+    while start <= end:
+        yield start
+        start += 1
 
 print(list(genrange(1, 10)))
